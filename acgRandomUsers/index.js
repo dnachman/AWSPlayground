43373cbb92839f3013444cc0d39d3ee1c3f818c1
David Nachman <david@nachman.org>
2021-05-19 22:12:20 +0000

    added api, parsing of data, good progress on random-users

--- acgRandomUsers/index.js
+++ acgRandomUsers/index.js
@@ -1,18 +1,35 @@
 exports.handler = async (event) => {
-	// TODO implement
 	
 	const AWS = require('aws-sdk');
 
 	const client = new AWS.S3({region: 'us-east-1'});
 	
+	const envBucket = process.env.Bucket;
+	
 	const data = await client.listObjectsV2({
-		Bucket: 'acg-logicalenigma-2021',
+		Bucket: envBucket,
 		Prefix: 'lambda-example/random-users-data/users'
 	}).promise();
 	
-	console.log('DATA: ' + JSON.stringify(data));
+	//console.log('DATA: ' + JSON.stringify(data));
 	
-	var output = data.Contents;
+	let output = [];
+	
+	for(const keyData of data.Contents) {
+		console.log('Key: ' + keyData.Key);
+		// grab the object and add it to the 
+		const fileData = await client.getObject({
+			Bucket: envBucket,
+			Key: keyData.Key
+		}).promise();
+		const fileJson = JSON.parse(fileData.Body.toString());
+		for(const user of fileJson) {
+			output.push(user);
+			//console.log('USER: ' + user.name.last);
+		}
+		// const fileJson = JSON.parse(fileData);
+		// output.append(fileJson);
+	}
 	
 	const response = {
 			statusCode: 200,
